b7fc3f2da44f6141c154205c395cbb0e323e3c77
jorgemartinezSQDM <jorgemartinez@sqdm.com>
2022-07-05 10:51:34 -0500

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "CCM API app",
-      script: "./bin/www",
+      script: "./app.js",
       instances: "max",
       env: {
         NODE_ENV: "development",
